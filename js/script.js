6f05ff40c90b711c07fe6085804d171d7b260182
xuansonkrt <43500435+xuansonkrt@users.noreply.github.com>
2018-11-11 10:44:52 +0700

    tabs+spinner

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-$(document).ready(function(){
+
+jQuery(document).ready(function($){
     $("#clickMe").click(function(){
         alert("click me");
         this.hide();
     });
-    $(".fa-stack").mouseover(function(){
+    $(".fa-stack").hover(function(){
+        $(this).find("i:first").addClass("color-4");
+        $(this).find(".icon").addClass("color-3");
+    },function(){
+        $(this).find(".circle").removeClass("color-4");
+        $(this).find(".icon").removeClass("color-3");
+    }),
+    
+    $("#tab-ChiTiet").mouseover(function(){
         // alert("demo");
-        $(".circle").toggle("color-4");
-        //$("i:first").toggle("slow");
+        
     });
-    $(".fa-stack").mouseout(function(){
-        // alert("demo");
-        $(".circle").show("slow");
+    $("#tabs").tabs();
+    $("#SoLuong").spinner({
+        min:1,
+        max:100,
+        step:1
     });
 
 });
